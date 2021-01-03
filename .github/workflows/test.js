817bb1f8c678a514e9fc5ee4a147f648e5a66614
JPSO <jpso.guerreiro@gmail.com>
2021-01-03 15:59:07 -0300

    .

--- .github/workflows/test.js
+++ .github/workflows/test.js
@@ -3,10 +3,10 @@ const fs = require("fs")
 exec("cd..", () =>{})
 exec("cd..", ()=>{})
 exec("npx standard --fix", (error, stdout, stderr) => {
-	console.log(`stdout: ${stdout}`);
+	console.log(`${stdout}`);
 	let logs = stdout.split("\n")
 	for (i in logs){
-	console.log(i.split(".js")[0])
+	console.log(logs[i].split(".js")[0])
 } 
 const data = fs.readFileSync(".standard.config")
 if (logs.length > Number(data)){
