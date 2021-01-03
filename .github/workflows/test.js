94175b02f890ae173e9a2595624a9f3a6c2f2ada
JPSO <jpso.guerreiro@gmail.com>
2021-01-03 15:45:31 -0300

    a

--- .github/workflows/test.js
+++ .github/workflows/test.js
@@ -2,8 +2,9 @@ const { exec } = require("child_process");
 exec("cd..", () =>{})
 exec("cd..", ()=>{})
 exec("npx standard --fix", (error, stdout, stderr) => {
+	console.log(`stdout: ${stdout}`);
     if (stdout.split("\n").length > 1){
 	process.exit(1)	
 	}
-    console.log(`stdout: ${stdout}`);
+   
 });
